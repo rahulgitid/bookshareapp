7142e1d695ea37074d9b3ff1d743ad5a1e0ec8df
rahulgitid <rahulguptaid87@gmail.com>
2026-02-07 22:56:32 +0530

    call navigation component from main activity

--- app/src/main/java/com/bksapp/bookshare/ui/MainActivity.kt
+++ app/src/main/java/com/bksapp/bookshare/ui/MainActivity.kt
@@ -3,12 +3,16 @@ package com.bksapp.bookshare.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import com.bksapp.bookshare.navigation.AppNavigation
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContent {  }
+        setContent {
+
+            AppNavigation()
+        }
     }
 }
