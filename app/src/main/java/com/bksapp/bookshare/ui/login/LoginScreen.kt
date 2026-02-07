44cc3054320bd212f93b28f4f1513ed9a1bb0b95
rahulgitid <rahulguptaid87@gmail.com>
2026-02-07 22:02:54 +0530

    create login screen

---
+++ app/src/main/java/com/bksapp/bookshare/ui/login/LoginScreen.kt
@@ -0,0 +1,20 @@
+package com.bksapp.bookshare.ui.login
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.material3.OutlinedTextField
+
+@Composable
+fun LoginScreen() {
+    Box(modifier = Modifier
+        .fillMaxSize()
+    ){
+        Column(modifier = Modifier
+            .fillMaxSize()) {
+
+        }
+    }
+}
