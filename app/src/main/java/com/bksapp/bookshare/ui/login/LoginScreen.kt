e3b8f67144fa35bc71acc48c9a7e8ce340403d0e
rahulgitid <rahulguptaid87@gmail.com>
2026-02-08 14:26:04 +0530

    update login screen UI

--- app/src/main/java/com/bksapp/bookshare/ui/login/LoginScreen.kt
+++ app/src/main/java/com/bksapp/bookshare/ui/login/LoginScreen.kt
@@ -1,28 +1,33 @@
 package com.bksapp.bookshare.ui.login
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material3.ElevatedButton
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.graphics.TileMode
+import androidx.compose.ui.platform.LocalWindowInfo
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 
@@ -31,10 +36,18 @@ fun LoginScreen() {
 
     var emailState by rememberSaveable { mutableStateOf("") }
     var passwordState by rememberSaveable { mutableStateOf("") }
-
+    val configuration = LocalWindowInfo.current.containerSize
 
     Box(modifier = Modifier
         .fillMaxSize()
+        .background(
+            brush = Brush.radialGradient(
+                colors = listOf(Color.Cyan,Color.Green,Color.Cyan),
+                center = Offset(configuration.width/2f, configuration.height/2f),
+                radius = configuration.width.toFloat(),
+                tileMode = TileMode.Clamp
+            )
+        )
     ){
         Column(modifier = Modifier
             .fillMaxSize()
@@ -69,19 +82,34 @@ fun LoginScreen() {
                     label = {Text("Password", color = Color.Red)},
                     visualTransformation = PasswordVisualTransformation(),
                     maxLines = 1
-
-                    )
+                )
             }
 
             Row(modifier = Modifier
                 .fillMaxWidth()
-                .padding(20.dp),
+               ,
                 horizontalArrangement = Arrangement.Center) {
                 ElevatedButton(modifier = Modifier
                     .width(200.dp)
                     .height(50.dp),
-                    onClick = {}) {
-                    Text("Login")
+                    onClick = {},
+                    contentPadding = PaddingValues(0.dp)) {
+                    Box(
+                        modifier = Modifier
+                            .background(Brush.verticalGradient(listOf(Color.Red,Color.LightGray,Color.Black))) // Apply the gradient to the Box
+                            .fillMaxWidth()
+                            .padding(horizontal = 16.dp, vertical = 16.dp), // Add your desired padding
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Text(
+                            "Login",
+                            style = TextStyle(
+                                brush = Brush.horizontalGradient(listOf(Color.Magenta,Color.Red)),
+                                fontWeight = FontWeight.ExtraBold
+                            )
+                        )
+                    }
+
                 }
             }
 
