1b5f97e41a94054f9af537af554184dace28e6ec
rahulgitid <rahulguptaid87@gmail.com>
2026-02-21 17:38:18 +0530

    add home navigation

--- app/src/main/java/com/bksapp/bookshare/navigation/AppNavigation.kt
+++ app/src/main/java/com/bksapp/bookshare/navigation/AppNavigation.kt
@@ -1,34 +1,51 @@
 package com.bksapp.bookshare.navigation
 
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.bksapp.bookshare.ui.dashboard.HomeScreen
+import com.bksapp.bookshare.ui.login.LoginEvent
 import com.bksapp.bookshare.ui.login.LoginScreen
 import com.bksapp.bookshare.ui.signup.SignUpScreen
 
 @Composable
-fun AppNavigation(){
+fun AppNavigation(padding: PaddingValues){
 
     val navController = rememberNavController()
 
-    NavHost(navController, startDestination = AppRoutes.Login.getRoute()) {
+    NavHost(navController, startDestination = AppRoutes.Login.getRoute(),
+        modifier = Modifier.padding(padding)) {
 
         composable(
             route = AppRoutes.Login.getRoute()
         ) {
-            LoginScreen{
-                navController.navigate(AppRoutes.Signup.getRoute()){
+            val loginEvent = LoginEvent(navController)
+            LoginScreen(
+                onLogin = loginEvent::loginSuccess,
+                onSignup = loginEvent::signupTextClicked
+            )
+        }
+
+        composable(
+            route = AppRoutes.Signup.getRoute()
+        ) {
+            SignUpScreen{
+                navController.navigate(AppRoutes.Login.getRoute()){
                     navController.popBackStack()
                 }
             }
         }
 
         composable(
-            route = AppRoutes.Signup.getRoute()
-        ) {
-            SignUpScreen()
+            route = AppRoutes.Home.getRoute()
+        ){
+            HomeScreen()
         }
+
     }
 }
 
