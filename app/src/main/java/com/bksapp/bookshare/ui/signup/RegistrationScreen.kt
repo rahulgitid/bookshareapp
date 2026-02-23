2db04367c5c87458cd7de7c5b6e0641dad3c5ccc
rahulgitid <rahulguptaid87@gmail.com>
2026-02-23 14:57:07 +0530

    update viewModel get with hilt lifecycle

--- app/src/main/java/com/bksapp/bookshare/ui/signup/RegistrationScreen.kt
+++ app/src/main/java/com/bksapp/bookshare/ui/signup/RegistrationScreen.kt
@@ -38,7 +38,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
-import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.bksapp.bookshare.R
 import java.time.Instant
