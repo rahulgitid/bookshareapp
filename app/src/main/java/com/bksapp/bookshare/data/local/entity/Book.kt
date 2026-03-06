81320b7c53d82122249cde9104a3ab958da181c4
rahulgitid <rahulguptaid87@gmail.com>
2026-03-06 21:26:37 +0530

    update book detail image UI, add shadow and add share and favourite button

--- app/src/main/java/com/bksapp/bookshare/data/local/entity/Book.kt
+++ app/src/main/java/com/bksapp/bookshare/data/local/entity/Book.kt
@@ -1,5 +1,8 @@
 package com.bksapp.bookshare.data.local.entity
 
+import androidx.compose.runtime.Immutable
+
+@Immutable
 data class Book(
     val id : Int,
     val title : String,
--- app/src/main/java/com/bksapp/bookshare/ui/bookdetail/BookDettailScreen.kt
+++ app/src/main/java/com/bksapp/bookshare/ui/bookdetail/BookDettailScreen.kt
@@ -1,21 +1,44 @@
 package com.bksapp.bookshare.ui.bookdetail
 
+import android.view.WindowInsets
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material.icons.filled.IosShare
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.text.font.FontWeight
@@ -27,12 +50,15 @@ import com.bksapp.bookshare.data.local.entity.Book
 import com.bksapp.bookshare.data.repository.NetworkStatus
 import com.bksapp.bookshare.ui.dashboard.HomeViewModel
 import com.bksapp.bookshare.utils.ImageLoader
+import com.bksapp.bookshare.utils.cardStyle
+import com.bksapp.bookshare.utils.roundedCard
 
 
 @Composable
 fun BookDetails(bookID : Int){
     val bookDetailViewModel = hiltViewModel<BookDetailViewModel>()
     val bookState = bookDetailViewModel.bookState.collectAsStateWithLifecycle()
+    var favoriteState  by remember { mutableStateOf(false)}
     LaunchedEffect(Unit) {
 
         bookDetailViewModel.getBook(bookID)
@@ -51,7 +77,7 @@ fun BookDetails(bookID : Int){
         }
         else if(bookState.value is NetworkStatus.Success){
            val book = bookState.value as NetworkStatus.Success<Book>
-            BookView(book.data)
+            BookView(book.data,favoriteState,{favoriteState = favoriteState.not()})
         }
     }
 
@@ -59,20 +85,39 @@ fun BookDetails(bookID : Int){
 }
 
 @Composable
-fun BookView(book : Book){
-    Box() {
-
-        Card(modifier = Modifier
-            .fillMaxWidth()
-            .height(300.dp),
-            shape = RectangleShape
+fun BookView(book : Book, favorite: Boolean, clickFavorite:()->Unit)
+{
+    Row() {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth().padding(20.dp),
+            contentAlignment = Alignment.Center
         ) {
-            ImageLoader(book.cover)
+            ElevatedCard(
+                modifier = Modifier.size(200.dp, 300.dp),
+                shape = RoundedCornerShape(12.dp),
+                elevation = CardDefaults.cardElevation(30.dp)
+            ) {
+                ImageLoader(book.cover)
+            }
+            Column(modifier = Modifier.align(Alignment.TopEnd)) {
+                IconButton(onClick = {}) {
+                    Icon(Icons.Filled.IosShare, contentDescription = "")
+                }
+                if(favorite) {
+                    IconButton(onClick = clickFavorite) {
+                        Icon(Icons.Filled.Favorite, contentDescription = "")
+                    }
+                }
+
+                else{
+                    IconButton(onClick = clickFavorite) {
+                        Icon(Icons.Filled.FavoriteBorder, contentDescription = "")
+                    }
+                }
+            }
         }
 
-        Row(modifier = Modifier
-            .wrapContentWidth().align(Alignment.BottomEnd).padding(4.dp)) {
-            Text(text = book.title, color = Color.White, fontSize = 16.sp, fontWeight = FontWeight.Bold)
-        }
     }
+
 }
